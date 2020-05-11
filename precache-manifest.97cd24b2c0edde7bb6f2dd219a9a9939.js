5fff9d0f358c0600d17961e741ccf6f2752a91fc
Rm4n <arm1377arman@gmail.com>
2020-05-11 10:43:51 +0430

    Updates

---
+++ precache-manifest.97cd24b2c0edde7bb6f2dd219a9a9939.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421baee08ce54cafaf56e235a3f7ac34",
+    "url": "/CookingApp/index.html"
+  },
+  {
+    "revision": "65561aba3ed87f3bacb6",
+    "url": "/CookingApp/static/css/main.8845b870.chunk.css"
+  },
+  {
+    "revision": "9e03cd4e62e0e434515f",
+    "url": "/CookingApp/static/js/2.43358060.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CookingApp/static/js/2.43358060.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65561aba3ed87f3bacb6",
+    "url": "/CookingApp/static/js/main.5a4a9be7.chunk.js"
+  },
+  {
+    "revision": "b0a54e6f4e4b5f34c61c",
+    "url": "/CookingApp/static/js/runtime-main.3695d109.js"
+  }
+]);
